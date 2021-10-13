3b2ec679a959b2997626f4f32ef58d7a07c1c219
Лейля Зиннатуллина <91633395+LeilyaZinnatullina@users.noreply.github.com>
2021-10-13 21:44:41 +0300

    Add files via upload

---
+++ Лейля Зиннатуллина4.cpp
@@ -0,0 +1,17 @@
+﻿//Напишите функцию, которая вычисляет N-ое число Фибоначчи.
+#include<iostream>
+using namespace std;
+
+int main() {
+	int N,c;
+	cin >> N;
+	int a = 1, b = 1;
+	for (c = 1; c <= N; c++) {
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	cout << "N" << "=" << c << endl;
+	return 0;
+}
+
